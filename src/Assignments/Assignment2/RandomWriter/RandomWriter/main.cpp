19b74533e8ee9bb526c581e0df8160a5589f88a7
bdlane <brendan@bdlane.com>
2020-10-09 21:44:48 +0100

    Add RandomWriter base project

---
+++ src/Assignments/Assignment2/RandomWriter/RandomWriter/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  RandomWriter
+//
+//  Created by Brendan Lane on 09/10/2020.
+//
+
+#include <iostream>
+#include "console.h"
+
+using namespace std;
+
+int main() {
+    // insert code here...
+    std::cout << "Hello, World!\n" << endl;
+    return 0;
+}
