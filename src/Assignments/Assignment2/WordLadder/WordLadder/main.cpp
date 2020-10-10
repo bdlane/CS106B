bc5864687a28e2d3c6edc9ffc10476b15cd08ef8
bdlane <brendan@bdlane.com>
2020-10-10 16:14:55 +0100

    Add WordLadder base project

---
+++ src/Assignments/Assignment2/WordLadder/WordLadder/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  WordLadder
+//
+//  Created by Brendan Lane on 10/10/2020.
+//
+
+#include <iostream>
+#include "console.h"
+
+using namespace std;
+
+int main() {
+    // insert code here...
+    std::cout << "Hello, World!" << endl;
+    return 0;
+}
